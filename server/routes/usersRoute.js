bdb7af54a2376a36a4846129e48203c7135aa92f
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-15 16:42:42 -0700

    adds users routes and controllers

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import{
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/usersController.js';
+
+const router = express.Router();
+
+router.get("/:id", getUser);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
