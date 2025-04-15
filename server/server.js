e359acf9e69665da9b7051654a077ab713d51170
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-15 16:21:18 -0700

    completed server.js update for production code

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
@@ -6,11 +8,16 @@ import spicesRoute from './routes/spicesRoute.js'
 // import usersRoute from './routes'
 import visionRoute from './routes/visiontempRoute.js'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json()) 
+
+const REACT_BUILD_DIR = path.join(__dirname, "..", "client", "dist");
+app.use(express.static(REACT_BUILD_DIR));
 
 //to read the tables
 app.use('/spices', spicesRoute); 
@@ -18,7 +25,6 @@ app.use('/spices', spicesRoute);
 // app.use('/users', usersRoute); 
 
 app.use('/vision', visionRoute); 
-
 
 
 app.get('/', (req,res) => res.send("Hello! This is the homepage!")); //test connection to the home page
