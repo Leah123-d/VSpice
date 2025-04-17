a88e2f62ba73894b24b42baaaba1c7dc87fb8740
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-17 12:41:49 -0700

    adds submit for image upload

--- client/src/components/CreateSpice.jsx
+++ client/src/components/CreateSpice.jsx
@@ -1,15 +1,40 @@
 //adjust to use, use reducer
-function CreateSpice(){
+//Other componenets are going to use the stored data, not the initial fetch data.
+//I think I will be fetching the image analyzer from here 
+import { useState } from 'react'
+
+function CreateSpice({createNewSpice}){
+  const [spiceImage, setSpiceImage] = useState(null);
+
+  const handleImageUpload = (e) =>{
+    const file = e.target.files[0];
+    setSpiceImage(file);  
+    console.log(typeof file);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!spiceImage){
+      alert("please upload an image first");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("spiceImage", spiceImage);
+    console.log("data from handleSubmit", formData)
+
+    createNewSpice(formData);
+  }
   return(
-   
     <div className="flex align-middle justify-center">
-      <form >
+      <form onSubmit={handleSubmit} >
         <label htmlFor="spiceImage">Spice Image Upload</label>
         <div>
-        <input type="file" id="spiceImage" ></input>
+        <input type="file" id="spiceImage" onChange={handleImageUpload}></input>
         </div>
         <div>
-          <button className="outline-2 rounded-md p-2">upload</button>
+          <button type="submit" className="outline-2 rounded-md p-2">upload</button>
         </div>
       </form>
       </div>
