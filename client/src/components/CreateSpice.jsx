7005e687a22ee1865697cb7245e3c1e9280afe6a
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-17 16:59:34 -0700

    adds image upload preview

--- client/src/components/CreateSpice.jsx
+++ client/src/components/CreateSpice.jsx
@@ -7,32 +7,32 @@ function CreateSpice({createNewSpice}){
   const [spiceImage, setSpiceImage] = useState(null);
 
   const handleImageUpload = (e) =>{
-    const file = e.target.files[0];
-    setSpiceImage(file);  
-    console.log(typeof file);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if(!spiceImage){
-      alert("please upload an image first");
-      return;
-    }
-
     const formData = new FormData();
-    formData.append("spiceImage", spiceImage);
-    console.log("data from handleSubmit", formData)
-
-    createNewSpice(formData);
+    formData.append("file", e.target.files[0]);
+    setSpiceImage(e.target.files[0]);
+    e.target.value = null;
   }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   if(!spiceImage){
+  //     alert("please upload an image first");
+  //     return;
+  //   }
+
+  //   console.log("data from handleSubmit", formData)
+
+  //   createNewSpice(formData);
+  // }
   return(
     <div className="flex align-middle justify-center">
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <form /*onSubmit={handleSubmit}*/ encType="multipart/form-data">
+        {spiceImage && <img src={URL.createObjectURL(spiceImage)}/>}
         <label htmlFor="spiceImage">Spice Image Upload</label>
         <div>
           {/* can hide input and use label to make the upload window to appear */}
-        <input type="file" id="spiceImage" onChange={handleImageUpload}></input>
+        <input type="file" id="spiceImage" accept="image/*" onChange={handleImageUpload}></input>
         </div>
         <div>
           <button type="submit" className="outline-2 rounded-md p-2">upload</button>
