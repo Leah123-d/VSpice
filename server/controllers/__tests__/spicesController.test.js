4cf3441589762eb9bd39c3b8cad85e72df4c9c40
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-22 12:07:40 -0700

    test for spice GET request

---
+++ server/controllers/__tests__/spicesController.test.js
@@ -0,0 +1,30 @@
+import dbConnection from "server/db-connection.js";
+import supertest from "supertest";
+import express from "express";
+import dotenv from "dotenv";
+import { getSpices } from "../spicesController";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use("/spices", getSpices);
+
+const request = supertest(app);
+
+beforeAll(async () => {
+  await dbConnection.connect();
+});
+
+describe("GET /spices", () => {
+  test("should successfully reach GET request and check first spice", async () => {
+    const res = await request
+      .get("/spices")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined(); //confirm spices are not undefined
+    expect(res.body[0]).toHaveProperty("name");
+  });
+});
