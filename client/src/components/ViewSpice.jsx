49231f61bebf71df9e9dec3b21fb75c7ed477d41
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-23 12:57:09 -0700

    sets up spice data display

--- client/src/components/ViewSpice.jsx
+++ client/src/components/ViewSpice.jsx
@@ -1,32 +1,15 @@
-import { useState } from "react";
+function ViewSpice({ viewSpice }) {
+  console.log('in view spice component', viewSpice)
 
-function ViewSpice({ spiceAnalyze, isLoading }) {
-  const [showSpice, setShowSpice] = useState(false);
-  const handleClick = (e) => {
-    e.preventDefault();
-    setShowSpice(true);
-  };
   return (
     <div>
-      {isLoading && (
-        <p>Loading...</p>
+      {viewSpice && viewSpice.map(spice => 
+        <ul key={spice.id}>
+          <li>{spice.name}</li>
+          <li>{spice.brand}</li>
+        </ul>
       )}
-      {spiceAnalyze && !isLoading &&
-        (<button
-          onClick={handleClick}
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-        >
-          View Spice
-        </button>
-      )}
-      {showSpice &&  (
-        <p>
-          Spice Brand: {spiceAnalyze.brand}
-          Total Grams Full: {spiceAnalyze.total_grams_full}
-          Expiration_date:{" "}
-          {spiceAnalyze.expiration_date ? spiceAnalyze.expiration_date : "none"}
-        </p>
-      )}
+      
     </div>
   );
 }
