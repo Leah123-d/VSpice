a2b0001d4dbc43988b771b701a1dade81b06bc4e
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-22 17:50:00 -0700

    deletes seed file

--- server/seed.sql
+++
@@ -1,9 +0,0 @@
--- Insert “starter” rows
-INSERT INTO spices (
-  name, brand, full_weight, current_weight,
-  expiration_date, last_purchased, notes,
-  inactive, user_id
-)
-VALUES
-  ('Paprika', 'store', 30, 5, NULL, NULL, NULL, FALSE, 1),
-  ('Cumin',   'organic', 20, 9, NULL, NULL, NULL, FALSE, 1);
