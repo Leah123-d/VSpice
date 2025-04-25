93442c953b2960ad1c7105d9ccb8b515bfe5b523
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-25 14:23:34 -0700

    updates db-connection for deploy

--- server/db-connection.js
+++ server/db-connection.js
@@ -4,13 +4,9 @@ import pkg from 'pg';
 dotenv.config();
 const { Pool } = pkg;
 
-const Database = new Pool({
-    host: process.env.PGHOST,
-    port: Number(process.env.PGPORT),
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD
-});
+const Database = new Pool ({
+    connectionString: process.env.DATABASE_URI
+})
 
 Database.connect()
     .then(() => console.log("connected to the database"))
