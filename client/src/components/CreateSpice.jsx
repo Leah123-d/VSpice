8b43f9e65bf85437b45564ebb7f4455e78b4b278
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-29 13:06:03 -0700

    adds validation for image formats

--- client/src/components/CreateSpice.jsx
+++ client/src/components/CreateSpice.jsx
@@ -1,14 +1,39 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-function CreateSpice({ createNewSpice, isLoading, getSpices, isAnalyzing, setIsLoading, setIsAnalyzing, newSpiceId }) {
-
+function CreateSpice({
+  createNewSpice,
+  isLoading,
+  getSpices,
+  isAnalyzing,
+  setIsLoading,
+  setIsAnalyzing,
+  newSpiceId,
+}) {
   const navigate = useNavigate();
   const location = useLocation();
+  const allowedImageFormats = [
+    "image/png",
+    "image/jpeg",
+    "image/gif",
+    "image/webp",
+  ];
   const [spiceImage, setSpiceImage] = useState(null);
+  const [imageFormatError, setImageFormatError] = useState(false);
 
   const handleUpload = (e) => {
     const file = e.target.files[0];
+    console.log("file in file upload", file.size, file.type);
+    if (file.size > 5000000) {
+      alert("File size exceeds 5MB");
+      return;
+    }
+    if (!allowedImageFormats.includes(file.type)) {
+      setImageFormatError(true);
+      return;
+    } else{
+      setImageFormatError(false);
+    }
     setSpiceImage(file);
     createNewSpice(file);
     e.target.value = null;
@@ -16,12 +41,27 @@ function CreateSpice({ createNewSpice, isLoading, getSpices, isAnalyzing, setIsL
   useEffect(() => {
     setIsLoading(true);
     setIsAnalyzing(false);
-  }, [location.pathname,setIsLoading,setIsAnalyzing]);
+  }, [location.pathname, setIsLoading, setIsAnalyzing]);
 
   return (
     <div>
       <h1>AI Spice Analyze</h1>
-      <section id="analyze-section" className="flex justify-center items-center h-screen bg-gray-50">
+      <section
+        id="analyze-section"
+        className="flex justify-center items-center h-screen bg-gray-50"
+      >
+        {imageFormatError && (
+          <div
+            class="mt-2 bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg p-4 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500"
+            role="alert"
+            tabindex="-1"
+            aria-labelledby="hs-soft-color-danger-label"
+          >
+            <span id="hs-soft-color-danger-label" class="font-bold">
+            Unsupported image. Please make sure your image has of one the following formats: ['png', 'jpeg', 'gif', 'webp'].
+            </span>{" "}
+          </div>
+        )}
         <div className="image-container">
           {spiceImage && <img src={URL.createObjectURL(spiceImage)} />}
         </div>
