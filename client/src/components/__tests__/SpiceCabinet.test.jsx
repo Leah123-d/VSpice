93a72f248d99e00f4aadead23b5a57a877351161
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-05-01 11:29:52 -0700

    completes tests for spice cabinet

--- client/src/components/__tests__/SpiceCabinet.test.jsx
+++ client/src/components/__tests__/SpiceCabinet.test.jsx
@@ -1,14 +1,15 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import { expect, test, afterEach } from "@jest/globals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { expect, test, afterEach, jest } from "@jest/globals";
+import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import SpiceCabinet from "../SpiceCabinet";
 
 afterEach(() => {
   cleanup();
 });
 
-test("renders spice cabinet component", async () => {
+test("should render heading of spice cabinet component", async () => {
   render(
     <BrowserRouter>
       <SpiceCabinet />
@@ -16,4 +17,23 @@ test("renders spice cabinet component", async () => {
   );
 
   expect(screen.getByRole("heading")).toHaveTextContent("Spice Cabinet");
+});
+
+test("should navigate to view spice component", async () => {
+
+  const mockedGetSpices = jest.fn();
+  const mockedSpices = [{id: 1, name: "tumeric", brand: "Whole Foods"},{id: 2, name: "ginger", brand: "Safeway"}]
+  const mockedDisplaySpices = [{id: 1, name: "tumeric", brand: "Whole Foods"},{id: 2, name: "ginger", brand: "Safeway"}]
+  const mockedDeleteSpice = jest.fn();
+
+  render(
+    <BrowserRouter>
+      <SpiceCabinet getSpices={mockedGetSpices} storedSpices={mockedSpices} displayedSpices={mockedDisplaySpices} deleteSpice={mockedDeleteSpice}/>
+    </BrowserRouter>
+  );
+
+  const viewButton = screen.getAllByRole("button", {name:/view-one-spice/i})[0];
+  await userEvent.click(viewButton);
+
+  expect(mockedGetSpices).toHaveBeenCalled();
 });
