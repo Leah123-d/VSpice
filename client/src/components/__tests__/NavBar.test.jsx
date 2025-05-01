707dd358eed75d57cb73f53a825572691bb7ba58
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-04-30 23:02:16 -0700

    adds text for navbar router

--- client/src/components/__tests__/NavBar.test.jsx
+++ client/src/components/__tests__/NavBar.test.jsx
@@ -1,23 +1,44 @@
-import {render, screen, cleanup} from '@testing-library/react'
-import { expect, test, afterEach } from "@jest/globals"
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
+import { expect, test, afterEach } from "@jest/globals";
 import "@testing-library/jest-dom";
-import { BrowserRouter, jest } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
-import '@testing-library/jest-dom'
-import NavBar from '../NavBar'
+import "@testing-library/jest-dom";
+import NavBar from "../NavBar";
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
 
-test('loads and displays header ', async () => {
-  
+test("loads and displays header ", async () => {
   render(
-  <BrowserRouter>
-  <NavBar  />
-  </BrowserRouter>)
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>
+  );
 
-  const createSpiceButton = screen.getAllByText(/Create new Spice/i) 
+  const createSpiceButton = screen.getAllByText(/Create new Spice/i);
 
   expect(createSpiceButton[0]).toBeInTheDocument();
-})
+});
+
+test("create spice navigates to create spice page", async () => {
+
+  render(
+      <MemoryRouter initialEntries={['/']}>
+        <NavBar to="/create" />
+        <Routes>
+          <Route path='/' element={<p>home</p>} />
+          <Route path='/create' element={<p>create spice page</p>} />
+        </Routes>
+      </MemoryRouter>
+  );
+
+  const createlink = screen.getAllByText(/Create new Spice/i);
+
+  await userEvent.click(createlink[0]);
+
+  await waitFor(() => {
+    expect(screen.getByText(/create spice page/i)).toBeInTheDocument();
+  })
+});
