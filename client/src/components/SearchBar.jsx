a9aafeaf5143470b008efe04202dad656253771f
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-05-06 19:41:52 -0700

    updates search bar to recognize enter input

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -30,7 +30,11 @@ function SearchBar({ setSearchResult }) {
     setSearchSpice("");
     setSearchResult("");
   };
-
+  const handleEnterKey = (e) =>{
+    if(e.key === 'Enter'){
+      handleSubmit();
+    }
+  }
   return (
     <div className="flex justify-end">
       <div className="max-w-xs">
@@ -43,6 +47,7 @@ function SearchBar({ setSearchResult }) {
             placeholder="Type a spice name"
             value={searchSpice}
             onChange={(e) => handleChange(e.target.value)}
+            onKeyDown={handleEnterKey}
           />
           <button
             className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-hidden focus:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:bg-blue-800/30 dark:hover:bg-blue-800/20 dark:focus:bg-blue-800/20"
